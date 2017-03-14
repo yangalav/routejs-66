2746e1a5ff3c27d18762aa30374968065526c081
Cameron <camdunne@gmail.com>
2017-03-14 00:25:03 -0400

    further moduralized and added angular functionality

---
+++ public/map/map.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+// module.exports = function initMap() {
+//   var uluru = {lat: 40.730610, lng:  -73.935242};
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 4,
+//     center: uluru
+//   });
+//   var marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map
+//   });
+// }
