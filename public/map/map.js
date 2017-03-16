40967bed7b4f19e62f92a3ba5f6566356ca65b16
Viswada Yangala <yangalav@go.stockton.edu>
2017-03-15 20:43:16 -0400

    map successfully shows route polyline between start and end locations

--- public/map/map.js
+++ public/map/map.js
@@ -2,58 +2,41 @@ angular.module('mapApp.map', [])
 .controller('mapCtrl', function($rootScope, $scope, $window) {
   console.log($rootScope.lat, $rootScope.lng)
   var uluru = {lat: $rootScope.lat, lng: $rootScope.lng}
+
+  var manhattan = new google.maps.LatLng(40.7711329, -73.9741874);
   $window.map = new google.maps.Map(document.getElementById('map'), {
+    control: {},
     zoom: 12,
-    center: uluru
+    center: manhattan
   });
   $window.marker = new google.maps.Marker({
-    position: uluru,
+    position: manhattan,
     map: map
   });
 
-  var directionsService = new google.maps.DirectionsService;
-  // var directionsDisplay = new google.maps.DirectionsRenderer();
-  // var geocoder = new google.maps.Geocoder();
-  //
-  // $scope.directions = {
-  //   origin: "Collins St, Melbourne, Australia",
-  //   destination: "MCG Melbourne, Australia",
-  //   showList: false
-  // }
-  //
-  // $scope.getDirections = function () {
-  //     var request = {
-  //       origin: $scope.directions.origin,
-  //       destination: $scope.directions.destination,
-  //       travelMode: google.maps.DirectionsTravelMode.DRIVING
-  //     };
-  //     directionsService.route(request, function (response, status) {
-  //       if (status === google.maps.DirectionsStatus.OK) {
-  //         directionsDisplay.setDirections(response);
-  //         directionsDisplay.setMap($window.map.control.getGMap());
-  //         // directionsDisplay.setPanel(document.getElementById('directionsList'));
-  //         // $scope.directions.showList = true;
-  //       } else {
-  //         alert('Google route unsuccesfull!');
-  //       }
-  //     });
-  //   }
 
-  // $window.onChangeHandler = function() {
-  //         calculateAndDisplayRoute($window.directionsService, $window.directionsDisplay);
-  //       };
-  //       document.getElementById('start').addEventListener('change', onChangeHandler);
-  //       document.getElementById('end').addEventListener('change', onChangeHandler);
-  // }
-  // var request = {
-  //   origin: 'Chicago',
-  //   destination: 'New York',
-  //   travelMode: google.maps.DirectionsTravelMode.DRIVING
-  // };
-  //
-  // directionsService.route(request, function(response, status) {
-  //   if (status === google.maps.DirectionsStatus.OK) {
-  //     directionsDisplay.setDirections(response);
-  //   }
-  // });
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer({map: $window.map});
+  var stepDisplay = new google.maps.InfoWindow;
+
+  $scope.request = {
+      origin: $scope.origin,
+      destination: $scope.destination,
+      travelMode: 'WALKING'
+    };
+
+  $scope.getDirections = function () {
+      console.log($scope.request);
+      directionsService.route($scope.request, function (response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+        directionsDisplay.setMap($window.map);
+        // directionsDisplay.setPanel(document.getElementById('directionsList'));
+        // $scope.directions.showList = true;
+      } else {
+        alert('Does not work bruh');
+      }
+      });
+    }
+
 });
